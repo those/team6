e7a302806391098036e4d82600184672710b60a1
Eduardo Cucharro <ecucharro@uolinc.com>
2019-08-21 14:40:53 -0300

    resolução dos problemas de path manipulation

--- src/main/java/dbProcs/Constants.java
+++ src/main/java/dbProcs/Constants.java
@@ -1,10 +1,21 @@
 package dbProcs;
 
 import java.io.File;
+import java.io.IOException;
 
 public final class Constants {
 	public static final String CATALINA_BASE = System.getProperty("catalina.base");
-	public static final String CATALINA_CONF = CATALINA_BASE + File.separator + File.separator + "conf";
+	public static final String CATALINA_CONF = getBasePath() + File.separator + File.separator + "conf";
 	public static final String DBPROP = CATALINA_CONF + File.separator + "database.properties";
 	public static final String SETUP_AUTH = CATALINA_CONF + File.separator + "SecurityShepherd.auth";
+
+	private static String getBasePath() {
+		String path = "";
+		try {
+			path = new File("./").getCanonicalPath();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return path;
+	}
 }
